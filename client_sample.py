a6ee6f202c501e57f4caab0d26098800ed85e658
Lakshmi Meghana Chekka <108412535+Chlmeghana@users.noreply.github.com>
2025-03-22 10:56:02 +0530

    Update client_sample.py

--- client_sample.py
+++ client_sample.py
@@ -1,8 +1,9 @@
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python3 your_script.py <value>")
+if len(sys.argv) != 2 or not sys.argv[1].strip():
+    print("Error: No value received from Jenkins.")
     sys.exit(1)
 
 selected_value = sys.argv[1]
 print(f"Selected Value: {selected_value}")
+
