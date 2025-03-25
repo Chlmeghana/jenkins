4add9568e178f1fbd86c435c03b28b1c267bc2ef
Lakshmi Meghana Chekka <108412535+Chlmeghana@users.noreply.github.com>
2025-03-25 12:10:34 +0530

    Update rsclient2.py

--- rsclient2.py
+++ rsclient2.py
@@ -10,7 +10,7 @@ available_hatt_files = sys.argv[2]
 name = sys.argv[3]
 password = sys.argv[4]
 # Number to send
-number = f"macbook meghana 1 {name} {password} {available_hatt_files}"  # Change this to any number
+number = f"macbook meghana 2 {name} {password} {available_hatt_files}"  # Change this to any number
 
 #
 def receive_lines(sock):
