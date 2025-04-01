d9f46b6bffbd8e29d8ffe6058ade9ada1942b60a
Lakshmi Meghana Chekka <108412535+Chlmeghana@users.noreply.github.com>
2025-04-01 12:14:16 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,11 @@
+import os
+
 names = ["Alice", "Bob", "Charlie"]
-with open("/tmp/names.txt", "w") as f:
+path = "/tmp/names.txt"
+
+with open(path, "w") as f:
     for name in names:
         f.write(name + "\n")
 
-# Optional: print for Jenkins console
-for name in names:
-    print(name)
+print(f"✅ File created: {os.path.exists(path)}")
+print(f"✅ Written to: {path}")
