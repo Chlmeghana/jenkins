90b6ebb9bf1cbec2e24b0e7de27cd442e75fb68a
Lakshmi Meghana Chekka <108412535+Chlmeghana@users.noreply.github.com>
2025-04-04 17:30:34 +0530

    Update rsclient.py

--- rsclient.py
+++ rsclient.py
@@ -9,7 +9,7 @@ lpars_selection = sys.argv[1]
 available_hatt_files = sys.argv[2]
 name1 = sys.argv[3]
 name2 = sys.argv[4]
-password = sys.argv[5]
+password = "CF7TEST5"
 # Number to send
 number = f"macbook {name1} 1 {name2} {password} {available_hatt_files}"
 
